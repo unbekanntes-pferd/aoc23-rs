4eb8680b1d32641c55d66e5c6fa554ea531dbd63
Octavio Simone <o.simone@dracoon.com>
2023-12-17 19:10:45 +0100

    solve day 11 part 2

--- src/day11.rs
+++ src/day11.rs
@@ -5,9 +5,34 @@ struct Galaxy(usize, usize);
 
 impl Galaxy {
     fn distance(&self, other: &Galaxy) -> usize {
-        // distance in 2D matrix 
+        // distance in 2D matrix
         (self.0 as isize - other.0 as isize).abs() as usize
             + (self.1 as isize - other.1 as isize).abs() as usize
+    }
+
+    fn shift_x(&mut self, x_shift_indices: &Vec<usize>, scale: Option<usize>) {
+        let scale = scale.unwrap_or(1);
+
+        let shift = x_shift_indices
+            .iter()
+            .filter(|&&shift_index| shift_index < self.0)
+            .map(|_| if scale == 1 { 1 } else { scale - 1 })
+            .sum::<usize>();
+
+        self.0 = (self.0 as isize + shift as isize) as usize;
+    }
+
+
+    fn shift_y(&mut self, y_shift_indices: &Vec<usize>, scale: Option<usize>) {
+        let scale = scale.unwrap_or(1);
+
+        let shift = y_shift_indices
+            .iter()
+            .filter(|&&shift_index| shift_index < self.1)
+            .map(|_| if scale == 1 { 1 } else { scale - 1 })
+            .sum::<usize>();
+
+        self.1 = (self.1 as isize + shift as isize) as usize;
     }
 }
 
@@ -58,6 +83,31 @@ fn solve_part1(input: &str) -> usize {
         .sum::<usize>()
 }
 
+fn solve_part_2(input: &str) -> usize {
+    solve_by_shift(input, 1000_000)
+}
+
+fn solve_by_shift(input: &str, scale: usize) -> usize {
+    let space = parse_input(input);
+
+    let mut galaxies = get_galaxies(&space);
+
+    let (empty_rows, empty_cols) = get_empty_space(&space);
+
+    galaxies.iter_mut().for_each(|galaxy| {
+        galaxy.shift_x(&empty_cols, Some(scale));
+        galaxy.shift_y(&empty_rows, Some(scale));
+    });
+
+    let galaxy_pairs = get_galaxy_pairs(&galaxies);
+
+    galaxy_pairs
+        .iter()
+        .map(|(g1, g2)| g1.distance(g2))
+        .sum::<usize>()
+}
+
+
 fn get_galaxies(space: &Vec<Vec<SpaceField>>) -> Vec<Galaxy> {
     space
         .iter()
@@ -83,6 +133,35 @@ fn get_galaxy_pairs(galaxies: &Vec<Galaxy>) -> HashSet<(Galaxy, Galaxy)> {
     }
 
     pairs
+}
+
+fn get_empty_space(space: &Vec<Vec<SpaceField>>) -> (Vec<usize>, Vec<usize>) {
+    let empty_rows = space
+        .iter()
+        .enumerate()
+        .filter(|(_, row)| row.iter().all(|field| field == &SpaceField::Space))
+        .map(|(i, _)| i)
+        .collect::<Vec<usize>>();
+
+    // get values of a column
+    let mapped_col_vals = (0..space[0].len())
+        .map(|i| {
+            space
+                .iter()
+                .map(|row| row[i].clone())
+                .collect::<Vec<SpaceField>>()
+        })
+        .collect::<Vec<_>>();
+
+    // find empty columns
+    let empty_cols = mapped_col_vals
+        .iter()
+        .enumerate()
+        .filter(|(_, row)| row.iter().all(|field| field == &SpaceField::Space))
+        .map(|(i, _)| i)
+        .collect::<Vec<usize>>();
+
+    (empty_rows, empty_cols)
 }
 
 fn parse_input(input: &str) -> Vec<Vec<SpaceField>> {
@@ -143,7 +222,12 @@ fn expand_space(space: &mut Vec<Vec<SpaceField>>) -> Vec<Vec<SpaceField>> {
 
 fn main() {
     let input = include_str!("assets/day11/input");
-    solve_part1(input);
+    let result = solve_part1(input);
+    println!("Part 1: {}", result);
+
+    let result = solve_part_2(input);
+    println!("Part 2: {}", result);
+
 }
 
 #[cfg(test)]
@@ -208,7 +292,21 @@ mod tests {
     fn test_part1() {
         let input = include_str!("assets/day11/input_test");
         let sum = solve_part1(input);
-
         assert_eq!(sum, 374);
     }
+
+    #[test]
+    fn test_part1_by_shifting() {
+        let input = include_str!("assets/day11/input_test");
+        let sum = solve_by_shift(input, 1);
+        assert_eq!(sum, 374);
+    }
+
+    #[test]
+    fn test_solving_with_10x_expand() {
+        let input = include_str!("assets/day11/input_test");
+        let sum = solve_by_shift(input, 10);
+        assert_eq!(sum, 1030);
+    }
+
 }
